d65dc306491cc3d4ee47fdf37d4020326b0abb5a
Chiomcee <cigwe072013@gmail.com>
2023-09-07 04:05:39 +0100

    correct output

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,37 +1,13 @@
 #!/usr/bin/node
-/*
-* Defines a Square class that inherits from the previous Square class
-* with instance charPrint()
-*/
-
-//const Square = require('./5-square');
-// Import the Square class from 5-square.js
-/*
-class Square2 extends Square {
-    charPrint(c) {
-        if (c === undefined) {
-            c = 'X';
-        }
-        for (let n = 0; n < this.height; n++) {
-            console.log(c.repeat(this.width));
-        }
-    }
-}
-
-module.exports = Square;
-
-*/
-
-
 /**
- * Square class that inherits from previous square class
+ * Defines a Square class that inherits from previous square class
  */
-const PrevSquare = require('./5-square');
+const BaseSquare = require('./5-square');
 
-class Square extends PrevSquare {
+/* class Square extends BaseSquare {
   charPrint (c) {
     const myChar = c === undefined ? 'X' : c;
-    for (let i = 0; i < this.height; i++) {
+    for (let n = 0; n < this.height; n++) {
       let myVar = '';
       let y = 0;
       while (y < this.width) {
@@ -40,6 +16,26 @@ class Square extends PrevSquare {
       }
 
       console.log(myVar);
+    }
+  }
+}
+
+module.exports = Square; */
+
+const BaseSquare = require('./5-square');
+
+class Square extends BaseSquare {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      for (let n = 0; n < this.height; n++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
     }
   }
 }
