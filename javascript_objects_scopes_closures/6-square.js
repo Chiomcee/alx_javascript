10a415c6247ebf8079d8ddfa793747e5041e14a0
Chiomcee <cigwe072013@gmail.com>
2023-09-08 08:41:45 +0100

    update

--- javascript_objects_scopes_closures/6-square.js
+++ javascript_objects_scopes_closures/6-square.js
@@ -1,42 +1,16 @@
 #!/usr/bin/node
-/**
- * Defines a Square class that inherits from previous square class
- */
-const BaseSquare = require('./5-square');
-
-/* class Square extends BaseSquare {
-  charPrint (c) {
-    const myChar = c === undefined ? 'X' : c;
-    for (let n = 0; n < this.height; n++) {
-      let myVar = '';
-      let y = 0;
-      while (y < this.width) {
-        myVar += myChar;
-        y++;
-      }
-
-      console.log(myVar);
-    }
-  }
-}
-
-module.exports = Square; */
-
 const BaseSquare = require('./5-square');
 
 class Square extends BaseSquare {
-  constructor (size) {
-    super(size, size);
-  }
+  charPrint(c) {
+    let character = c || 'X';
+    let square = '';
 
-  charPrint (c) {
-    if (c) {
-      for (let n = 0; n < this.height; n++) {
-        console.log(c.repeat(this.width));
-      }
-    } else {
-      this.print();
+    for (let i = 0; i < this.height; i++) {
+      square += character.repeat(this.width) + '\n';
     }
+
+    console.log(square);
   }
 }
 
