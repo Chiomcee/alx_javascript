d6115d87142667c3543b6dfc42d36a5850f8cdfa
Chiomcee <cigwe072013@gmail.com>
2023-09-14 02:22:34 +0100

    correct output

--- javascript-network/1-starwars_title.js
+++ javascript-network/1-starwars_title.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
 // Script that prints the title of a Star Wars movie
 
+
 const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-const id =process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/${id}';
-
-request.get(url, function(error, response, body) {
-    if (error) {
-        console.log(error);
-    } else {
-        const movie =JSON.parse(body);
-        console.log(movie.title);
-    }
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
+  }
 });
