98fe368c8f4931fe8235c8eade404ecd2c055780
Chiomcee <cigwe072013@gmail.com>
2023-09-14 04:00:45 +0100

    update

--- javascript-network/3-request_store.js
+++ javascript-network/3-request_store.js
@@ -1,23 +1,20 @@
 #!/usr/bin/node
 // Script that gets the contents of a webpage and stores it
 
+
 const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
-const filePath = process.argv[3];
+const file = process.argv[3];
 
-request.get(url, function (error, response, body) {
+request(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('API Error:', response.statusCode);
+    console.log(error);
   } else {
-    fs.writeFile(filePath, body, 'utf-8', function (err) {
-      if (err) {
-        console.error('Error writing to file:', err);
-      } else {
-        console.log('File stored successfully!');
+    fs.writeFile(file, body, 'utf8', (error) => {
+      if (error) {
+        console.log(error);
       }
     });
   }
